a7626cf52608272c236b0027ad3e33c4113da02b
benterkidhiaeddine <benterki.dhiaeddine@gmail.com>
2023-01-01 17:11:09 +0100

    Create the functions that evaluates the result of playing a round of rock paper scissors

--- game.js
+++ game.js
@@ -1,22 +1,63 @@
 //declare the choices list
 let choices = ["rock","paper","scissors"];
 
+//function that generates a random int between 0 and max(exclusive)
+function getRandomInt(max){
+    return Math.floor(Math.random()*max);
+}
+
 // function for computer choice
 function getComputerChoice(){
    
     randomNumber =getRandomInt(3);
     return choices[randomNumber]
 }
-//function that generates a random int between 0 and max(exclusive)
-function getRandomInt(max){
-    return Math.floor(Math.random()*max);
-}
-console.log(getComputerChoice())
+
+
 
 //function that plays one round of rock paper scissors
-    //variable to store the computer choice
+function playRound(playerChoice,computerChoice){
+    if ((playerChoice.toLowerCase() ==="rock") && (computerChoice==="paper")){
+        return "Computer wins!";
 
+    }
+    else if ((playerChoice.toLowerCase() ==="paper") && (computerChoice==="rock")){
+        return "Player wins!";
 
-    //variable to store the player choice
+    }
+    else if ((playerChoice.toLowerCase() ==="rock") && (computerChoice==="scissors")){
+        return "Player wins!";
+
+    }
+    else if ((playerChoice.toLowerCase() ==="scissors") && (computerChoice==="paper")){
+        return "Player wins!";
+
+    }
+    else if ((playerChoice.toLowerCase() ==="scissors") && (computerChoice==="rock")){
+        return "Computer wins!";
+
+    }
+    else if ((playerChoice.toLowerCase() ==="paper") && (computerChoice==="scissors")){
+        return "Computer wins!";
+
+    }
+    else if ((playerChoice.toLowerCase() ==="paper") && (computerChoice==="paper")){
+        return "It's a draw!";
+
+    }
+    else if ((playerChoice.toLowerCase() ==="rock") && (computerChoice==="rock")){
+        return "It's a draw!";
+
+    }
+    else if ((playerChoice.toLowerCase() ==="scissors") && (computerChoice==="scissors")){
+        return "It's a draw!";
+
+    }
+
+    return
+
+}
+
+console.log(playRound("rock",getComputerChoice()));
 
 
